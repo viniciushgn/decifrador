42b9c67c9bd12c32e52376a4a881cf1c74521a73
vinihg <v188182@dac.unicamp.br>
2020-08-15 18:10:11 -0300

    Função codificadora invertida.

--- src/main.c
+++ src/main.c
@@ -6,14 +6,44 @@
 
 #include <stdio.h>
 
-#define TAM_BUFFER 50
+#define TAM_BUFFER 60
 char buffer_entrada[TAM_BUFFER];
 char buffer_saida[TAM_BUFFER];
+const char senha[] = "SENHASECRETA";
+
+
+char soma_modulo_alfabeto_reversa(char a, char b) {
+  char a0, b0, c0;
+  if (a>='A' && a<='Z') {
+    a0 = a-'A';
+    b0 = b-'A';
+
+//------------------------------------------------------------------------------
+    c0 = 'A'+(a0-b0+('Z'-'A'+1))%('Z'-'A'+1);
+//sabendo que o método de encriptação pode ser definido como (M+K+Const)%X,
+//podemos inverter a transformacao simplesmente voltando (subtraindo) ao inves
+//de avancando (somando) a partir da chave, (M-K+Const)%X.
+//'Voltando' e 'Avancando' se refere a mecanica do disco de Vigenere.
+//------------------------------------------------------------------------------
+    return c0;
+  } else return a;
+}
 
 int main() {
+  int j=0;
+  int i=0;
+
   /* Leitura da entrada */
   fgets(buffer_entrada, TAM_BUFFER, stdin);
 
+
+  for (i=0; (i<TAM_BUFFER) && (buffer_entrada[i]!='\0') && (buffer_entrada[i]!='\n'); i++) {
+
+    buffer_saida[i] = soma_modulo_alfabeto_reversa(buffer_entrada[i], senha[j]);
+    j++;
+    if (senha[j]=='\0') j=0;
+  }
+  buffer_saida[i]='\0';
 
 
   /* Escrever saida na tela */
